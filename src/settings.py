146749a14d8a1d63b30faddd721c3d2b3a72875d
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-12 21:55:11 -0500

    Update debug

--- src/settings.py
+++ src/settings.py
@@ -26,8 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+DEBUG_OPTIONS = [True, False]
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG_OPTIONS[int(os.environ.get("DEBUG"))]
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
